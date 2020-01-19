c9e85428ec5b79b0b334a544d160c34bca830a1d
Ted <tconn@salesforce.com>
2020-01-19 12:54:06 +0100

    moved all font loading to the app component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-      <Header />
-      <Intro />
-      <Footer />
+    <Header />
+    <Intro />
+    <Footer />
   </div>
 </template>
 
@@ -23,41 +23,73 @@ export default class App extends Vue {}
 </script>
 
 <style>
+.container {
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+@media (min-width: 576px) {
   .container {
-    width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
+    max-width: 540px;
   }
-  @media (min-width: 576px) {
-    .container {
-      max-width: 540px;
-    }
+}
+@media (min-width: 768px) {
+  .container {
+    max-width: 672px;
   }
-  @media (min-width: 768px) {
-    .container {
-      max-width: 672px;
-    }
+}
+@media (min-width: 992px) {
+  .container {
+    max-width: 896px;
   }
-  @media (min-width: 992px) {
-    .container {
-      max-width: 896px;
-    }
+}
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1104px;
   }
-  @media (min-width: 1200px) {
-    .container {
-      max-width: 1104px;
-    }
-  }
+}
 
+.container {
+  position: relative;
+  padding: 0 24px;
+}
+@media (min-width: 576px) {
   .container {
-    position: relative;
-    padding: 0 24px;
+    padding: 0;
   }
-  @media (min-width: 576px) {
-    .container {
-      padding: 0;
-    }
-  }
+}
+
+@font-face {
+  font-family: "Gelion";
+  font-display: block;
+  src: local("Gelion-Black"), url("./assets/fonts/Gelion-Black.woff2") format("woff2");
+  font-weight: 900;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Gelion";
+  font-display: block;
+  src: local("Gelion-Medium"), url("./assets/fonts/Gelion-Medium.woff2") format("woff2");
+  font-weight: 500;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Gelion";
+  font-display: block;
+  src: local("Gelion-Regular"), url("./assets/fonts/Gelion-Regular.woff2") format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Gelion";
+  font-display: block;
+  src: local("Gelion-Bold"), url("./assets/fonts/Gelion-Bold.woff2") format("woff2");
+  font-weight: bold;
+  font-style: normal;
+}
 </style>
